654d674eb66a4c24803c555ab0d7b2e63db4812c
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-06 14:32:35 -0600

    Setting up GH Pages

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './NavMenu.css'
 
