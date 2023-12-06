d22abe0b9d15da03bd88544a2f6d4728623cf68d
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-06 11:25:15 -0600

    Services Page Created & Finished

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,9 +1,56 @@
-function Services(){
-    return(
-        <div className="Services">
-            <h1>hi</h1>
-        </div>
-    )
+import "./Services.css";
+//Components
+import Offering from "./services-components/Offering";
+import GetInTouch from "../../components/Global/GetInTouch";
+//Images
+import Portrait1 from "../../assets/Images/Services Page/Portrait 1.jpg";
+import Portrait2 from "../../assets/Images/Services Page/Portrait 2.jpg";
+import Portrait3 from "../../assets/Images/Services Page/Portrait 3.jpg";
+import Portrait4 from "../../assets/Images/Services Page/Portrait 4.jpg";
+import Portrait5 from "../../assets/Images/Services Page/Portrait 5.jpg";
+
+import Wedding1 from '../../assets/Images/Services Page/Wedding 1.jpg';
+import Wedding2 from '../../assets/Images/Services Page/Wedding 2.jpg';
+import Wedding3 from '../../assets/Images/Services Page/Wedding 3.jpg';
+import Wedding4 from '../../assets/Images/Services Page/Wedding 4.jpg';
+import Wedding5 from '../../assets/Images/Services Page/Wedding 5.jpg';
+
+function Services() {
+  return (
+    <div className="Services">
+      <h1 id="services-header">What I Offer</h1>
+      <Offering
+        mainImage={Portrait1}
+        image1={Portrait2}
+        image2={Portrait3}
+        image3={Portrait4}
+        image4={Portrait5}
+        title={"Beautiful Moments\nIn Life"}
+        header={"PORTRAITS"}
+        content={
+          `I offer a wide range of different portrait shoots to meet my customers individual needs. The first option I offer is the “expedited shoots”, which includes a 20 minute shoot, online gallery, and a minimum of 25 edited photos for $250.\n\n\
+            The second option includes a 40-60 minute shoot, online gallery, and about 70-100 photos for $510.\n\n\
+            Please send me a message if you’d like to learn more about portrait shoots.`
+        }
+      />
+
+      <Offering
+        mainImage={Wedding5}
+        image1={Wedding1}
+        image2={Wedding2}
+        image3={Wedding3}
+        image4={Wedding4}
+        title={"Remember your story forever"}
+        header={"WEDDINGS"}
+        content={
+          "Sometimes simplicity is the best solution. Weddings are a day to be remembered as they unfold. Many photographers will try to stage things in a way that is almost unrealistic. My method is to let the day unfold naturally, and capture every moment as they happen.\n\n\
+          There are a few factors that go into pricing, but my customers spend on average $3,800 for a full 8 hour session and a second photographer. The smallest amount has been around $2,100. Contact me for more information."
+        }
+      />
+
+      <GetInTouch />
+    </div>
+  );
 }
 
-export default Services
+export default Services;
---
+++ src/pages/Services/services-components/Offering.tsx
@@ -0,0 +1,51 @@
+interface Props {
+  mainImage: any;
+  image1: any;
+  image2: any;
+  image3: any;
+  image4: any;
+  title: any;
+  header: any;
+  content: any;
+}
+
+function Offering(props: Props) {
+  return (
+    <div className="Offering">
+      <div className="offering-mobile">
+        <img src={props.mainImage} alt="" id="mainImage" />
+        <div className="Offering-Small-Images">
+          <img src={props.image1} alt="First Image" />
+          <img src={props.image2} alt="Second Image" />
+          <img src={props.image3} alt="Third Image" />
+          <img src={props.image4} alt="Fourth Image" />
+        </div>
+        <p id="offering-title">{props.title}</p>
+        <h1 id="offering-header">{props.header}</h1>
+        <pre>{<p id="offering-content">{props.content}</p>}</pre>
+        <button className="button">Contact Me</button>
+      </div>
+      <div className="offering-desktop">
+        <div className="offering-image-content">
+          <div className="offering-image-container">
+            <img src={props.mainImage} alt="" id="mainImage" />
+          </div>
+          <div className="offering-content-container">
+            <p id="offering-title">{props.title}</p>
+            <h1 id="offering-header">{props.header}</h1>
+            <pre>{<p id="offering-content">{props.content}</p>}</pre>
+            <button className="button">Contact Me</button>
+          </div>
+        </div>
+        <div className="Offering-Small-Images">
+          <img src={props.image1} alt="First Image" />
+          <img src={props.image2} alt="Second Image" />
+          <img src={props.image3} alt="Third Image" />
+          <img src={props.image4} alt="Fourth Image" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Offering;
