08d2c790bab27e8cd1df3f190d459a5dbd35a715
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-07 08:58:55 -0600

    ghpages update

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Jill-Johnsons-Portfolio/"
+  base: "/Jill-Johnsons-Portfolio"
 })
