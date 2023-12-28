23b130c6b1a754ec5172703d1c3c830e705cb2ce
Josh Smith <jsmith@dailyprinting.local>
2023-12-28 07:44:14 -0600

    Quick Fix
    Fixing some broken links, as well as a style issue on the contact page.

--- src/pages/Home/home-components/MeetMe.tsx
+++ src/pages/Home/home-components/MeetMe.tsx
@@ -11,7 +11,7 @@ function MeetMe() {
             <h1>Meet Jill</h1>
             <h3>Mom and photographer</h3>
             <p>Hi! I’m Jill. I’m a photographer and mother, and my passion is to make sure your memories are captured. I know first hand how important each and every moment is with your loved ones, and thats why capturing those moments mean so much to me.</p>
-            <Link id="meet-me-button" to={"/About"} className="button">Learn More About Me</Link>
+            <Link id="meet-me-button" to={"/Jill-Johnsons-Portfolio/About"} className="button">Learn More About Me</Link>
         </div>
       </div>
     </div>
